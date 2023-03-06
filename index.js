45a4adc519f2670273e03618187974a6e71e5d48
Bill Brown <bill.a.brown90@gmail.com>
2023-03-06 12:30:00 -0800

    updated mouseover eventlistener and a little css stylesheet

--- index.js
+++ index.js
@@ -12,16 +12,23 @@ fetch(BASE_API)
 
 const addToDrinkMenu = (drinkObj) => {
     console.log(drinkObj)
-const drinkList = document.querySelector("#drink-list")
+    const drinkList = document.querySelector("#drink-list")
 
-//creating a variable that takes image info from object
-drinkImg = document.createElement("img")
-drinkImg.src = drinkObj.strDrinkThumb
-drinkImg.id = drinkObj.idDrink
-drinkImg.alt = drinkObj.strDrink
-drinkImg.width = 200
-//append drink image to the drink Menu
-drinkList.append(drinkImg)
+    //creating a variable that takes image info from object
+    drinkImg = document.createElement("img")
+    drinkImg.src = drinkObj.strDrinkThumb
+    drinkImg.id = drinkObj.idDrink
+    drinkImg.alt = drinkObj.strDrink
+    drinkImg.width = 200
+    //append drink image to the drink Menu
+    drinkList.append(drinkImg)
+
+    //! Add an event "mouseover" event listener to drinkImg
+    drinkImg.addEventListener("mouseover",e => {
+        //create a div element within a specific id to go within image
+        console.log(drinkObj.strDrink)
+        drinkName = drinkObj.strDrink
+    })
 
 
 
