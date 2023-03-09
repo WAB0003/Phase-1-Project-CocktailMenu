20574943917cc286d1152994a4f22f691e77f04a
Bill Brown <bill.a.brown90@gmail.com>
2023-03-09 09:17:54 -0800

    clean up code and make as dry as possible

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
+//create a base URL that pulls from API data.
 const BASE_API = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
 
+//create an initial fetch function that fetches data from API
+const fetchData = () => {
 fetch(BASE_API)
     .then((resp) => resp.json())
     .then((data) => {
-        // console.log(data.drinks[0])
         data.drinks.forEach(drinkObj => {           
             addToDrinkMenu(drinkObj)
         })
     });
+}
     
-//*Create a Global Variable for the Drinklist area in our HTML
+//*Create a section of Global Variable that get used throughout the code. All variables are identifying places on original HTML
 const drinkList = document.querySelector("#drink-list")
+const drinkName = document.querySelector("#name")
+const centerImage  = document.querySelector('#image')
 const ingredientsTitle = document.querySelector("#ingredientsTitle")
 const ingredientList = document.querySelector("#ingredients")
 const drinkForm = document.getElementById("drink-form")
-const centerImage  = document.querySelector('#image')
 
-//!Create a function that takes and Drink Object and adds a it to the top Drink List
+//!Create a function that takes a Drink Object (either from API or form) and adds a it to the top Drink List
 const addToDrinkMenu = (drinkObj) => {
-    //Create a Span Element and assign and Id
+    //Create a Span Element and assign class and Id for future use
     const drinkListSpan = document.createElement("span")
     drinkListSpan.classList = "drinks"
     drinkListSpan.id = `d${drinkObj.idDrink}`
-    //Create an image variable in our HTML and set it to the Objects image source. Add Id, alt, and height.
+    //Create an image variable in HTML and set it to the Objects image source. Add Id, alt, and a standard height.
     drinkImg = document.createElement("img")
     drinkImg.src = drinkObj.strDrinkThumb
     drinkImg.id = `img${drinkObj.idDrink}`
@@ -31,84 +35,68 @@ const addToDrinkMenu = (drinkObj) => {
     //append drink image to the drink Menu
     drinkListSpan.append(drinkImg)
     drinkList.append(drinkListSpan)
-        
-    //! break out RenderImage function to create an event listener
+    
+    //!Create breakout functions that take information created in drink drink Menu and add Event Listeners to them:
+    //* break out RenderImage function to create an event listener
     renderImage(drinkImg,drinkObj)
-        
-    // ! Add an event "mouseover" & "mouseout" event listener to drinkImg
+    
+    //* Add an event "mouseover" & "mouseout" event listener to drinkImg
     mouseoverEvent(drinkListSpan,drinkObj,drinkImg)
     mouseoutEvent(drinkListSpan,drinkObj,drinkImg)
 }
     
-//! This is the break out RenderImage function
-    const renderImage = (drinkImg,drinkObj) => {
-        drinkImg.addEventListener('click', (event) => {
-            //create an ingredients Title
-            ingredientsTitle.textContent = "Ingredients"
-   
-            
+//! This is the break out RenderImage function to edit each menu item that is clicked.
+const renderImage = (drinkImg,drinkObj) => {
+    drinkImg.addEventListener('click', (event) => {
+        //Input a Title for the ingrediets list
+        ingredientsTitle.textContent = "Ingredients"
+        centerImage.src = event.target.src 
 
-            centerImage.src = event.target.src 
-            //add Drink Name to detail section
-            const drinkName = document.querySelector("#name")
+        //add Drink Name to detail section
+        drinkName.textContent = drinkObj.strDrink
 
-            drinkName.textContent = drinkObj.strDrink
+        //reset ingredient list and add new ingredients to list based on drink object selected
+        ingredientList.innerHTML = "";
 
-            //add ingredients to the detail section
-            ingredientList.innerHTML = "";
-            
-            const ingredientArray = [drinkObj.strIngredient1, drinkObj.strIngredient2, drinkObj.strIngredient3, drinkObj.strIngredient4, drinkObj.strIngredient5, drinkObj.strIngredient6, drinkObj.strIngredient7]
-            
-            ingredientArray.forEach(ingredient => {
-                if ((ingredient === null)||(ingredient === "")){
-                    return;
-                } else {
-                    const div = document.createElement("div")
-                    div.textContent = ingredient
-                    ingredientList.append(div)
-                }
-            });
-        });   
-    };
+        //create an Array for all ingredients needed required for a drink.
+        const ingredientArray = [drinkObj.strIngredient1, drinkObj.strIngredient2, drinkObj.strIngredient3, drinkObj.strIngredient4, drinkObj.strIngredient5, drinkObj.strIngredient6, drinkObj.strIngredient7]
+        ingredientArray.forEach(ingredient => {
+            if ((ingredient === null)||(ingredient === "")){
+                return;
+            } else {
+                const div = document.createElement("div")
+                div.textContent = ingredient
+                ingredientList.append(div)
+            }
+        });
+    });
+};
 
-//!Create a mouseover Event Listener that puts name in Image
-    const mouseoverEvent = (drinkListSpan, drinkObj,drinkImg) => {
-        drinkListSpan.addEventListener("mouseover", e => {
-            //create a span element within a specific id to go within image
+//!Create a mouseover Event Listener that puts name in Image and changes opacity
+const mouseoverEvent = (drinkListSpan, drinkObj,drinkImg) => {
+    drinkListSpan.addEventListener("mouseover", e => {
+        //create a span element within a specific id to go within image
+        const drinkImgName = document.createElement("span")
+        drinkImgName.textContent = drinkObj.strDrink
+        drinkImgName.classList = "drinkTitle"
+        drinkImgName.id = `span${drinkObj.idDrink}`
             
-            const drinkName = document.createElement("span")
-            drinkName.textContent = drinkObj.strDrink
-            drinkName.classList = "drinkTitle"
-            drinkName.id = `span${drinkObj.idDrink}`
-            
-            
-            drinkImg.style.opacity = 30/100
-            document.querySelector(`#d${drinkObj.idDrink}`).append(drinkName)
-        })
-    }
+        drinkImg.style.opacity = 30/100
+        document.querySelector(`#d${drinkObj.idDrink}`).append(drinkImgName)
+    })
+}
 
-    //!Create a mouseout Event Listener that puts name in Image
-    const mouseoutEvent = (drinkListSpan, drinkObj, drinkImg) => {
-        drinkListSpan.addEventListener("mouseout",e => {
-            //create a span element within a specific id to go within image
-            const drinkName = document.createElement("span")
-            drinkName.textContent = drinkObj.strDrink
-            drinkName.classList = "drinkTitle"
-            
-            drinkImg.style.opacity = 100/100
-            document.querySelector(`#span${drinkObj.idDrink}`).remove()
-        })
-    }
+//!Create a mouseout Event Listener that removes name from menu image and reverts original opacity 
+const mouseoutEvent = (drinkListSpan, drinkObj, drinkImg) => {
+    drinkListSpan.addEventListener("mouseout", e => {
+        drinkImg.style.opacity = 100/100
+        document.querySelector(`#span${drinkObj.idDrink}`).remove()
+    })
+}
 
-//Create an Event Listener for the Form
+//!Create an Event Listener for the Form
 drinkForm.addEventListener('submit', addNewDrink);
-
-// Tom workspace
-  
-   
- 
-
-
+//Create a function that takes information from form and adds it to our Menu!
 function addNewDrink(event) {
     event.preventDefault();
     console.log(event.target["drink-name"].value)
@@ -124,10 +112,8 @@ function addNewDrink(event) {
         strIngredient6: event.target["ingredient-6"].value,
     }
     drinkForm.reset()
-    console.log(newDrink)
     addToDrinkMenu(newDrink)
 }
-
 
 //! Creating reset button
 const resetBtn = document.getElementById("reset-btn")
@@ -136,6 +122,12 @@ resetBtn.addEventListener("click",e => {
     centerImage.src = "https://i2.wp.com/whatagirleats.com/wp-content/uploads/2017/06/Top-shot-Marg-Bar-2-hands.jpg"
     ingredientsTitle.textContent= ""
     ingredientList.innerHTML = ""
+    drinkName.textContent = ""
 })
 
- 
+
+//Initialize webapplication
+ const init = () => {
+    fetchData()
+ }
+ init()
